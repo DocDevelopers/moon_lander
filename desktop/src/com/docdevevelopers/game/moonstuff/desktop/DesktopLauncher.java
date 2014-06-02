d9e00550dc3a3b4fb2cfa17c957cb1fd69b5cc2c
Francisco Castellanos <francisco@327-C30.local>
2014-06-02 09:20:16 -0400

    Initial Upload

---
+++ desktop/src/com/docdevevelopers/game/moonstuff/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.docdevevelopers.game.moonstuff.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.docdevevelopers.game.moonstuff.MoonLander;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MoonLander(), config);
+	}
+}
---
+++ html/src/com/docdevevelopers/game/moonstuff/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.docdevevelopers.game.moonstuff.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.docdevevelopers.game.moonstuff.MoonLander;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MoonLander();
+        }
+}
